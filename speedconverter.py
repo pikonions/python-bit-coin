3a19d171453ba1f5fe66d005ac85dbb6dca7f49c
pikonions <sidhartakwanmiguel123@gmail.com>
2023-05-12 18:06:40 +0800

    k

---
+++ speedconverter.py
@@ -0,0 +1,8 @@
+kilo=input("masukan kecepatan tangan mu:")
+kilo=int(kilo)
+ms=(1000/3600)*kilo
+print("kecepatan tangan mu adalah",ms,"meter perdetik")
+jarak=input("masukan jarak kasur mu:")
+jarak=int(jarak)
+wtk=(jarak/kilo)
+print("kamu akan sampe ke kasur nyaman mu dalam waktu",wtk,"jam")
