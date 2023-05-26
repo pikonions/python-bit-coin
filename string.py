3f2adb7c15d34845125207795ea68dd74e6f73f2
pikonions <sidhartakwanmiguel123@gmail.com>
2023-05-26 17:55:22 +0800

    (˵ ͡° ͜ʖ ͡°˵)

---
+++ string.py
@@ -0,0 +1,9 @@
+namadepan="jason"
+namabelakang=" kwan"
+print(namadepan+namabelakang)
+print(namadepan*1000) 
+print(namadepan[2])
+print(namadepan[0:5])
+print("a"in namadepan)
+print("son"in namadepan)
+print("son"not in namadepan)
